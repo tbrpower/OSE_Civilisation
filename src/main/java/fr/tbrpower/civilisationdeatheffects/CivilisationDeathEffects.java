ce811db9d785fff3106f6e5c5843136a1e60b080
tbrpopwer <tbrpoweryt@gmail.com>
2026-01-17 14:26:04 +0100

    ban ip à la mort du joueur

--- src/main/java/fr/tbrpower/civilisationdeatheffects/CivilisationDeathEffects.java
+++ src/main/java/fr/tbrpower/civilisationdeatheffects/CivilisationDeathEffects.java
@@ -7,17 +7,24 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.time.Instant;
+
 public class CivilisationDeathEffects extends JavaPlugin implements  Listener {
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        getLogger().info("CivilisationDeathEffects activé");
+        getLogger().info("§9[CivilisationDeathEffects]§r Plugin civilisation activé !");
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         String playerName = event.getEntity().getName();
+        //String playerAdress = event.getPlayer().getAddress().getAddress().getHostAddress();
+        if (event.getEntity().getAddress() == null) return ;
+        event.getEntity().banIp("Vous êtes mort", Instant.now(), "Server", true);
+
+
         Bukkit.getServer().broadcast(Component.text("§cJoueur§r" + playerName));
     }
 }

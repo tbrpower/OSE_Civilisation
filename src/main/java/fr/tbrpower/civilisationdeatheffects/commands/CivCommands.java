55351fe976955175c1fdff7ed181354a91df0fc6
tbrpopwer <tbrpoweryt@gmail.com>
2026-01-18 02:57:04 +0100

    Ajouté commande /civ pardonall et fonction unbanDeadPlayers

--- src/main/java/fr/tbrpower/civilisationdeatheffects/commands/CivCommands.java
+++ src/main/java/fr/tbrpower/civilisationdeatheffects/commands/CivCommands.java
@@ -6,6 +6,7 @@ import org.apache.maven.model.Profile;
 import org.bukkit.BanEntry;
 import org.bukkit.BanList;
 import org.bukkit.Bukkit;
+import org.bukkit.ban.IpBanList;
 import org.bukkit.ban.ProfileBanList;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
@@ -23,13 +24,13 @@ public class CivCommands implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (args.length == 0) {
-            sender.sendMessage("Invalid syntax. Correct use : /civ <newday|list>");
+            sender.sendMessage("Invalid syntax. Correct use : /civ <pardonall|list>");
             return true;
         }
 
 
         switch (args[0].toLowerCase()) {
-            case "newday" -> sender.sendMessage("Unknown subcommand.");
+            case "pardonall" -> unbanDeadPlayers(sender);
             case "list" -> dumpDeadList(sender);
             default -> sender.sendMessage("Unknown subcommand.");
         }
@@ -41,7 +42,7 @@ public class CivCommands implements CommandExecutor, TabCompleter {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
         if (args.length == 1) {
-            return List.of("newday", "list")
+            return List.of("pardonall", "list")
                     .stream()
                     .filter(s -> s.startsWith(args[0].toLowerCase()))
                     .toList();
@@ -84,5 +85,28 @@ public class CivCommands implements CommandExecutor, TabCompleter {
             sender.sendMessage(MiniMessage.miniMessage().deserialize(sb.toString()
             ));
         }
+    }
+
+    public void unbanDeadPlayers(CommandSender sender) {
+        ProfileBanList profileBanList = (ProfileBanList) Bukkit.getBanList(BanList.Type.PROFILE);
+        IpBanList ipBanList = (IpBanList) Bukkit.getBanList(BanList.Type.IP);
+
+        ArrayList<String> revivedPlayers = new ArrayList<String>();
+
+        for (BanEntry<? super PlayerProfile> entry : profileBanList.getEntries()) {
+            PlayerProfile target = (PlayerProfile) entry.getBanTarget();
+            if (! (entry.getSource().isEmpty())  && entry.getSource().equals("nonPermDeath")) {
+                profileBanList.pardon(target);
+                ipBanList.pardon(target.getName());
+                revivedPlayers.add(target.getName());
+            }
+        }
+
+        if (revivedPlayers.isEmpty() ) {
+            sender.sendMessage("&cNo revivable dead players !&r");
+        } else {
+            sender.sendMessage("&aSuccesfully revived"+ revivedPlayers.size() + "players !&r");
+        }
+
     }
 }
